604217fdd2d66264d19fc477a9c05e695a8c4947
John Green <jpgreen@Johns-iMac.local>
2018-06-15 15:10:22 -0400

    Lesson 3

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by John Green on -06-152018.
+//  Copyright © 2018 John P. Green. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
